a630d32c42df85bb31cf5b7f07b27366022af792
Marvin Thapa Renteral <mtrenteral@gmail.com>
2021-01-25 22:44:14 -0500

    fixed few warnings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { render } from '@testing-library/react';
 import React from 'react'
 import './App.css';
 import NavBar from './Components/Navigation/NavBar/NavBar'
 import Footer from './Components/Footer/Footer'
-import HomePage from './Components/HomePage/HomePage'
-import ProductPage from './Components/ProductsPage/ProductPage'
 
 
 class App extends React.Component {
